f4284aee9e3a78317483ac1fefafa0e75deeb065
Similo <171865838+Chief-99@users.noreply.github.com>
2025-12-18 16:39:47 +0200

    feat:Create Book constructor for new book objects

---
+++ library.js
@@ -0,0 +1,14 @@
+function Book(title, author, pages, readStatus) {
+    if (!new.target) {
+        throw Error('You must use the new operator to call the constructor');
+    }
+
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+    this.info = function() {
+        let readOutput = readStatus === true ? 'has been read' : 'not read yet';
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${readOutput}`;
+    }
+}
