a4294ba600ac5e8845c3a144bf053290902b5b30
Similo <171865838+Chief-99@users.noreply.github.com>
2026-01-07 23:50:01 +0200

    style:Make responsive rows for book cards

--- library.js
+++ library.js
@@ -108,6 +108,11 @@ addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
 addBookToLibrary('Take The Risk', 'Ben Carson', 467, true);
 addBookToLibrary('Bad Science', 'Ben Goldacre', 370, true);
 addBookToLibrary('Gone Girl', 'Gillian Flynn', 475, true);
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
+addBookToLibrary('Take The Risk', 'Ben Carson', 467, true);
+addBookToLibrary('Bad Science', 'Ben Goldacre', 370, true);
+addBookToLibrary('Gone Girl', 'Gillian Flynn', 475, true);
+
 
 
 callDisplay();
